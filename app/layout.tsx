965f08ed37ef3256cad802200c39b63448eaeec0
mbenboulo <mackleodebenboulo@gmail.com>
2025-12-16 08:27:58 +0000

    add web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Poppins } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const inter = Inter({
@@ -37,6 +38,7 @@ export default function RootLayout({
       >
         <ModalProvider>
           {children}
+          <Analytics />
           <ContactModal />
         </ModalProvider>
       </body>
